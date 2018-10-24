d7b977fa3a6e103e602756341da530ed7b652047
Susanna Beglaryan <susanna.beglaryan@synisys.com>
2018-10-24 13:48:26 +0400

    first commit

---
+++ src/main/java/com/java2s/common/HelloBean.java
@@ -0,0 +1,22 @@
+package com.java2s.common;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+public class HelloBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
